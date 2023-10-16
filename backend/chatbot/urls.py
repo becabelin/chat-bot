dc231162b1eb0587911ba07baf3be582cead2ac8
Victor Medeiros <victormedeiros.pj@gmail.com>
2023-10-15 22:23:31 -0300

    backend finished

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -5,10 +5,15 @@ urlpatterns = [
     path('principal', views.obterMenuPrincipal, name='menu-principal'),
     path('plataforma', views.obterMenuPlataforma, name='menu-plataforma'),
     path('suporte', views.obterMenuSuporte, name='menu-suporte'),
-    path('suporte', views.obterMenuSuporte, name='menu-cancelamento'),
+    path('cancelamento', views.obterMenuCancelamento, name='menu-cancelamento'),
 
     path('plataforma/inicio', views.obterPlataformaInicio, name='plataforma-inicio'),
     path('plataforma/exercicios', views.obterPlataformaExercicios, name='plataforma-exercicios'),
     path('plataforma/videos', views.obterPlataformaVideos, name='plataforma-videos'),
     path('plataforma/tempo', views.obterPlataformaTempo, name='plataforma-tempo'),
+    path('plataforma/adicionais', views.obterPlataformaAdicionais, name='plataforma-adicionais'),
+
+    path('suporte/problemas', views.obterSuporteProblemas, name='suporte-problemas'),
+
+    path('cancelamento/politica', views.obterCancelamentoPolitica, name='cancelamento-politica'),
 ]
